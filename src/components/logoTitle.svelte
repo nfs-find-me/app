b0f8cebe423d32a8f4b9118864b61a1a9195f2c1
MaxenceBossin <maxbossin25@gmail.com>
2023-10-02 09:53:01 +0200

    création composant logoTitle

---
+++ src/components/logoTitle.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<div>
+	<img src="favicon.svg" alt="logo" />
+	<h1 class="xl">{title}</h1>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-bottom: 2rem;
+	}
+	h1 {
+		text-align: center;
+		font-size: 4rem;
+		color: blue;
+	}
+</style>
