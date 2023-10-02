f5b580b22590e51765f64bc474af6b956443dd10
MaxenceBossin <maxbossin25@gmail.com>
2023-10-02 21:57:00 +0200

    front form Inscription OK

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -10,15 +10,25 @@
 <section
 	class="container mx-auto bg-center flex flex-col justify-center items-center h-screen max-h-[80vh]"
 >
-	<LogoTitle title="connexion" />
+	<LogoTitle title="inscription" />
 
 	<form class="flex flex-col items-center justify-center">
 		<Input
 			config={{
-				type: 'text',
+				type: 'email',
 				id: 'email',
 				name: 'email',
-				label: "Email ou nom d'utilisateur",
+				label: 'Email',
+				text: ''
+			}}
+		/>
+
+		<Input
+			config={{
+				type: 'text',
+				id: 'username',
+				name: 'username',
+				label: "Nom d'utilisateur",
 				text: ''
 			}}
 		/>
@@ -33,12 +43,22 @@
 			}}
 		/>
 
+		<Input
+			config={{
+				type: 'password',
+				id: 'password_confirm',
+				name: 'password_confirm',
+				label: 'Confirmation du mot de passe',
+				text: ''
+			}}
+		/>
+
 		<p class="text-sm m-8 text-start">
-			Pas de compte ? <a class="text-blue underline" href="/register"
-				>Cliquez ici pour vous inscrire</a
+			Déjà un compte ? <a class="text-blue underline" href="/login"
+				>Cliquez-ici pour vous connecter</a
 			>
 		</p>
 
-		<Button fill={true} text={'Se connecter'} />
+		<Button fill={true} text={"S' inscrire"} />
 	</form>
 </section>
