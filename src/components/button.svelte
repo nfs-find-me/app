dc9b060c1236b0a4db5145612eddf06dc4374bbe
MaxenceBossin <maxbossin25@gmail.com>
2023-10-02 09:54:38 +0200

    Création composant button

---
+++ src/components/button.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let text = 'TEXT BUTTON A MODIFIER';
+	export let fill = true;
+</script>
+
+{#if fill}
+	<button
+		class="border border-blue bg-blue text-white rounded-full p-2
+	transition duration-300 ease-in-out hover:bg-blue_dark"
+	>
+		{text}
+	</button>
+{:else}
+	<button
+		class="border border-blue rounded-full p-2
+		transition duration-300 ease-in-out hover:bg-blue_light"
+	>
+		{text}
+	</button>
+{/if}
