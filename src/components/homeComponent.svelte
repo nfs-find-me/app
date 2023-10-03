d255ef93de6b0cd263704002962573c0aaeb89e8
j.pinto <j.pinto@pawa-consulting.com>
2023-10-03 14:59:09 +0200

    add VisitorHomeComponent

---
+++ src/components/homeComponent.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import VisitorHomeComponent from "./visitorHomeComponent.svelte";
+    export let isLogged:Boolean;
+
+    let welcomeImages: Array<{ src: string; handle: string }> = [
+        { src: 'image_2.jpg', handle: 'image_2' },
+        { src: 'image_5.jpg', handle: 'image_5' },
+        { src: 'image_6.webp', handle: 'image_6' },
+        { src: 'image_8.webp', handle: 'image_8' },
+        { src: 'image_9.webp', handle: 'image_9' },
+  ];
+</script>
+
+
+{#if isLogged}
+    <!-- TODO SI USER CONNECTE -->
+{:else}
+    <VisitorHomeComponent welcomeImages={welcomeImages}></VisitorHomeComponent>
+{/if}
+
