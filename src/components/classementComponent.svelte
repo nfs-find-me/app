b3fccfe9755b0124d8437fe21babbef76cba0d82
j.pinto <j.pinto@pawa-consulting.com>
2023-10-24 10:56:39 +0200

    :white_check_mark: classementComponent OK

---
+++ src/components/classementComponent.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import ErrorCard from "./errorCard.svelte";
+    import UserScoreComponent from "./userScoreComponent.svelte";
+
+    export let isLogged: Boolean;
+    let errorMessage = "Vous n'êtes pas autorisé à accéder à ce contenu.";
+    let onglet: string = 'global';
+
+    let userInfos: Array<{ username: string; src: string; score: number }> = [
+		{username: 'John Doe', src:'profil_picture.jpg', score: 1578},
+		{username: 'User 2', src:'profil_picture.jpg', score: 1420},
+		{username: 'User 3', src:'profil_picture.jpg', score: 1022},
+		{username: 'User 4', src:'profil_picture.jpg', score: 1000},
+	];
+
+    function sectionChange(newOnglet: string){
+        onglet = newOnglet;
+
+        if(onglet === 'global'){
+            userInfos = [
+                {username: 'John Doe', src:'profil_picture.jpg', score: 1578},
+                {username: 'User 2', src:'profil_picture.jpg', score: 1420},
+                {username: 'User 3', src:'profil_picture.jpg', score: 1022},
+                {username: 'User 4', src:'profil_picture.jpg', score: 1000},
+            ];
+        }else{
+            userInfos = [
+                {username: 'Follower1', src:'profil_picture.jpg', score: 1578},
+                {username: 'Follower2', src:'profil_picture.jpg', score: 1420},
+                {username: 'Follower3', src:'profil_picture.jpg', score: 1022},
+                {username: 'Follower4', src:'profil_picture.jpg', score: 1000},
+            ];
+        }
+    }
+
+</script>
+
+{#if isLogged}
+
+    <h3 class="w-11/12 my-10 mx-auto text-center text-xxl laptop:w-4/12">Vous êtes n°1 au classement général</h3>
+
+    <div class="w-full flex justify-center items-center">
+        <ul class="w-3/12 flex flex-row justify-center text-center">
+            <li class="w-full">
+                <span on:click={() => sectionChange('global')} class="inline-block w-full p-4 text-lg cursor-pointer {onglet === 'global' ? 'text-blue font-bold border-b-4 border-blue' : 'text-black'}">Général</span>
+            </li>
+            <li class="w-full">
+                <span on:click={() => sectionChange('follower')} class="inline-block w-full p-4 text-lg cursor-pointer {onglet === 'follower' ? 'text-blue font-bold border-b-4 border-blue' : 'text-black'}">Abonnement</span>
+            </li>
+        </ul>
+    </div>
+
+    <UserScoreComponent {userInfos}></UserScoreComponent>
+
+{:else}
+    <ErrorCard {errorMessage}></ErrorCard>
+{/if}
---
+++ src/components/errorCard.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+export let errorMessage: String;
+</script>
+
+<div class="w-4/5 p-5 mx-auto flex items-center mb-4 text-lg text-alert border border-alert rounded-lg bg-white-secondary" role="alert">
+    <div>
+        <i class="fa-solid fa-triangle-exclamation text-alert my-6"></i>
+        <span class="font-medium">{ errorMessage }</span>
+    </div>
+</div>
