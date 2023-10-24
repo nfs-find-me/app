bc347092a5182564ea3dfbeac898f3340a03c3d3
j.pinto <j.pinto@pawa-consulting.com>
2023-10-24 16:16:53 +0200

    :white_check_mark: settingComponent responsive OK

--- src/components/accountSettingComponent.svelte
+++ src/components/accountSettingComponent.svelte
@@ -4,12 +4,12 @@
 </script>
 
 <section id="accountInfo" class="flex flex-col w-full px-10 py-16 gap-14">
-    <div class="flex flex-col gap-4">
+    <div class="flex flex-col gap-8 tablet-sm:gap-4">
         <h2 class="text-xl font-bold text-blue">Information du compte</h2>
 
-        <div class="flex flex-row items-center justify-between">
+        <div class="flex flex-col gap-8 tablet-sm:gap-0 tablet-sm:flex-row items-center justify-between">
             <div>
-                <div class="flex flex-row gap-8 items-center">
+                <div class="flex flex-col tablet-sm:flex-row gap-8 items-center">
                     <img class="w-20 h-20 rounded-50 hover:cursor-pointer" src="profil_picture.jpg" alt="image de profil">
         
                     <div>
@@ -25,13 +25,13 @@
         </div>
     </div>
 
-    <div id="accountInfo" class="w-full flex flex-col gap-4">
+    <div id="accountInfo" class="w-full flex flex-col gap-8 tablet-sm:gap-4">
         <h2 class="text-xl font-bold text-blue">Suppression du compte</h2>
 
-        <div class="flex flex-row justify-between items-center">
+        <div class="flex flex-col gap-8 tablet-sm:gap-0 tablet-sm:flex-row justify-between items-center">
             <div class="flex flex-row gap-4 items-center">
-                <i class="fa-solid fa-triangle-exclamation text-lg text-alert"></i>
-                <p class="text-alert text-lg font-bold">Cette action est définitive et supprime toutes les données associées au compte.</p>
+                <i class="fa-solid fa-triangle-exclamation text-md tablet-sm:text-lg text-alert"></i>
+                <p class="text-alert font-bold text-md tablet-sm:text-lg">Cette action est définitive et supprime toutes les données associées au compte.</p>
             </div>
             <Button color={'red'} fill={true} text={'Supprimer'} />
         </div>
--- src/components/securitySettingComponent.svelte
+++ src/components/securitySettingComponent.svelte
@@ -11,14 +11,14 @@
         <p class="text-text-contrast text-md font-bold">Une fois le mot de passe modifié, vous serez redirigé vers la page de connexion où vous pourrez vous connecter avec votre nouveau mot de passe.</p>
     </div>
 
-    <div class="flex flex-col gap-2 w-2/4">
+    <div class="flex flex-col gap-2 w-full laptop:w-2/4">
         <Input label={'Mot de passe actuel'} type={InputTypeEnum.PASSWORD} id={'password'} name={'password'}/>
         <Input label={'Nouveau mot de passe'} type={InputTypeEnum.PASSWORD} id={'newPassword'} name={'newPassword'}/>
         <Input label={'Confirmez votre mot de passe'} type={InputTypeEnum.PASSWORD} id={'confirmPassword'} name={'confirmPassword'}/>
 
         <div class="flex flex-row justify-center items-center gap-6">
             <Button type={ButtonTypeEnum.SUBMIT} fill={true} text={"Enregistrer"} />
-            <a href="#" class="hover:text-blue hover:underline">J'ai oublié mon mot de passe</a>
+            <a href="#" class="hover:text-blue hover:underline text-center">J'ai oublié mon mot de passe</a>
         </div>
     </div>
 </section>
