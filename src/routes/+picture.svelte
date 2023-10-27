1fc39f01edefb3f70aea89780e0efbe8ef7e113b
loulou761001 <louis.cherik@gmail.com>
2023-10-27 10:30:12 +0200

    :sparkles: add mapbox and types

---
+++ src/routes/+picture.svelte
@@ -0,0 +1,1 @@
+import {Map} from "mapbox-gl"; import "../node_modules/mapbox-gl/dist/mapbox-gl.css"
---
+++ src/store/images.ts
@@ -0,0 +1,124 @@
+import type { PostType } from './types';
+
+export const welcomeImages: Array<PostType> = [
+	{
+		_id: '651d1b08e963f44c187b8046',
+		userId: 'test',
+		geolocation: {
+			posX: 111.12,
+			posY: -11.12,
+			zip: 76100,
+			address: 'address ici',
+			country: 'France'
+		},
+		picture: {
+			url: 'image_1',
+			thumbnail_url: 'image_1'
+		},
+		view: [
+			{
+				userId: '651fce18a3800637f1cecebc',
+				createdAt: new Date()
+			},
+			{
+				userId: '651fce18a3800637f1cecebc',
+				createdAt: new Date()
+			},
+			{
+				userId: '651fce18a3800637f1cecebc',
+				createdAt: new Date()
+			},
+			{
+				userId: '651fce18a3800637f1cecebc',
+				createdAt: new Date()
+			}
+		],
+		like: [],
+		verified: true,
+		createdAt: new Date(),
+		updatedAt: new Date()
+	},
+	{
+		_id: '651d1b08e963f44c187b8047',
+		userId: 'test',
+		geolocation: {
+			posX: 81.12,
+			posY: -21.12,
+			zip: 76100,
+			address: 'address ici',
+			country: 'France'
+		},
+		picture: {
+			url: 'image_2',
+			thumbnail_url: 'image_2'
+		},
+		view: [
+			{
+				userId: '651fce18a3800637f1cecebc',
+				createdAt: new Date()
+			}
+		],
+		like: [],
+		verified: true,
+		createdAt: new Date(),
+		updatedAt: new Date()
+	},
+	{
+		_id: '651d1b08e963f44c187b8048',
+		userId: 'test',
+		geolocation: {
+			posX: 121.12,
+			posY: -1.12,
+			zip: 76100,
+			address: 'address ici',
+			country: 'France'
+		},
+		picture: {
+			url: 'image_3',
+			thumbnail_url: 'image_3'
+		},
+		view: [
+			{
+				userId: '651fce18a3800637f1cecebc',
+				createdAt: new Date()
+			},
+			{
+				userId: '651fce18a3800637f1cecebc',
+				createdAt: new Date()
+			},
+			{
+				userId: '651fce18a3800637f1cecebc',
+				createdAt: new Date()
+			}
+		],
+		like: [],
+		verified: true,
+		createdAt: new Date(),
+		updatedAt: new Date()
+	},
+	{
+		_id: '651d1b08e963f44c187b8049',
+		userId: 'test',
+		geolocation: {
+			posX: 111.12,
+			posY: -11.12,
+			zip: 76100,
+			address: 'address ici',
+			country: 'France'
+		},
+		picture: {
+			url: 'image_4',
+			thumbnail_url: 'image_4'
+		},
+		view: [
+			{
+				userId: '651fce18a3800637f1cecebc',
+				createdAt: new Date()
+			}
+		],
+		like: [],
+		verified: true,
+		createdAt: new Date(),
+		updatedAt: new Date()
+	}
+];
