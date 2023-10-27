f8df18353fcacc68f555d7b00321a113032f8b8b
MaxenceBossin <maxbossin25@gmail.com>
2023-10-27 14:06:51 +0200

    *

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,14 @@
 import type { PageServerLoad } from './$types';
 import { CookiesHelper } from '$lib/helpers/cookies/cookies.helper';
+import { connected } from '$lib/store/isLogged';
 
 export const load = (async ({ cookies }) => {
 	try {
 		const cookiesHelper = new CookiesHelper(cookies);
 		await cookiesHelper.refreshCookies(cookies);
+		connected(true);
 	} catch {
+		connected(false);
 		return null;
 	}
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../style/button.css';
 	import '../style/input.css';
 	import '../style/animation.css';
+	import { isLogged } from '$lib/store/isLogged';
+	import { currentPage } from '$lib/store/pathStore';
 </script>
 
 <slot />
+<p>Test connexion : {$isLogged}</p>
+<p>Test connexion : {$currentPage}</p>
