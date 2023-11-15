92cace5e3ffd33c45127f174bdb213dae4471919
Alexis BRIET <alexis.briet2003@gmail.com>
2023-11-15 10:14:15 +0100

    Fix request and add admin base

--- src/lib/services/userService.ts
+++ src/lib/services/userService.ts
@@ -2,24 +2,36 @@ import type { Cookies } from '@sveltejs/kit';
 import { UserRestApi } from '../../api/feature/User.restAPI';
 import { CookiesHelper } from '../../helpers/cookies/cookies.helper';
 
-export class UserService
-{
-    private cookies:Cookies;
+export class UserService {
+	private cookies: Cookies;
 
-    constructor(cookies: Cookies) {
-        this.cookies = cookies;
-    }
+	constructor(cookies: Cookies) {
+		this.cookies = cookies;
+	}
 
-    public async getOne(){
-        const api = new UserRestApi(this.cookies);
-        const cookiesHelper = new CookiesHelper(this.cookies);
-        const userId = cookiesHelper.getUserId(this.cookies);
-    
-        if(userId){
-            const user = await api.getOne(userId);
-            return {user: user.data};
-        }else{
-            return {errorMessage: "Vous devez être connecté."}
-        }
-    }
+	public async getOne() {
+		const api = new UserRestApi(this.cookies);
+		const cookiesHelper = new CookiesHelper(this.cookies);
+		const userId = cookiesHelper.getUserId(this.cookies);
+
+		if (userId) {
+			const user = await api.getOne(userId);
+			return { user: user.data };
+		} else {
+			return { errorMessage: 'Vous devez être connecté.' };
+		}
+	}
+
+	public async getAll() {
+		const api = new UserRestApi(this.cookies);
+		const cookiesHelper = new CookiesHelper(this.cookies);
+		const userId = cookiesHelper.getUserId(this.cookies);
+
+		if (userId) {
+			const users = await api.getAll();
+			return { users: users.data };
+		} else {
+			return { errorMessage: 'Vous devez être connecté.' };
+		}
+	}
 }
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { User } from '../../model/user/User';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	const users = data.users as User[];
+	console.log(users);
+</script>
+
+{#each users as user}
+	<div>
+		{user.username}
+	</div>
+{/each}
