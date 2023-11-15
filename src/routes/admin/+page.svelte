310a6cd75b6ce254d68f169f7d21144b901aaaa6
Alexis BRIET <alexis.briet2003@gmail.com>
2023-11-15 12:10:34 +0100

    Update tailwind

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -11,116 +11,68 @@
 </script>
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-	<div class="mb-4 border-b border-gray-200 dark:border-gray-700">
-		<ul
-			class="flex flex-wrap -mb-px text-sm font-medium text-center"
-			id="default-tab"
-			data-tabs-toggle="#default-tab-content"
-			role="tablist"
-		>
-			<li class="me-2" role="presentation">
-				<button
-					class="inline-block p-4 border-b-2 rounded-t-lg"
-					id="profile-tab"
-					data-tabs-target="#profile"
-					type="button"
-					role="tab"
-					aria-controls="profile"
-					aria-selected="false">Profile</button
-				>
-			</li>
-			<li class="me-2" role="presentation">
-				<button
-					class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-					id="dashboard-tab"
-					data-tabs-target="#dashboard"
-					type="button"
-					role="tab"
-					aria-controls="dashboard"
-					aria-selected="false">Dashboard</button
-				>
-			</li>
-			<li class="me-2" role="presentation">
-				<button
-					class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-					id="settings-tab"
-					data-tabs-target="#settings"
-					type="button"
-					role="tab"
-					aria-controls="settings"
-					aria-selected="false">Settings</button
-				>
-			</li>
-			<li role="presentation">
-				<button
-					class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-					id="contacts-tab"
-					data-tabs-target="#contacts"
-					type="button"
-					role="tab"
-					aria-controls="contacts"
-					aria-selected="false">Contacts</button
-				>
-			</li>
-		</ul>
-	</div>
-	<div id="default-tab-content">
-		<div
-			class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-			id="profile"
-			role="tabpanel"
-			aria-labelledby="profile-tab"
-		>
-			<p class="text-sm text-gray-500 dark:text-gray-400">
-				This is some placeholder content the <strong
-					class="font-medium text-gray-800 dark:text-white">Profile tab's associated content</strong
-				>. Clicking another tab will toggle the visibility of this one for the next. The tab
-				JavaScript swaps classes to control the content visibility and styling.
-			</p>
-		</div>
-		<div
-			class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-			id="dashboard"
-			role="tabpanel"
-			aria-labelledby="dashboard-tab"
-		>
-			<p class="text-sm text-gray-500 dark:text-gray-400">
-				This is some placeholder content the <strong
-					class="font-medium text-gray-800 dark:text-white"
-					>Dashboard tab's associated content</strong
-				>. Clicking another tab will toggle the visibility of this one for the next. The tab
-				JavaScript swaps classes to control the content visibility and styling.
-			</p>
-		</div>
-		<div
-			class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-			id="settings"
-			role="tabpanel"
-			aria-labelledby="settings-tab"
-		>
-			<p class="text-sm text-gray-500 dark:text-gray-400">
-				This is some placeholder content the <strong
-					class="font-medium text-gray-800 dark:text-white"
-					>Settings tab's associated content</strong
-				>. Clicking another tab will toggle the visibility of this one for the next. The tab
-				JavaScript swaps classes to control the content visibility and styling.
-			</p>
-		</div>
-		<div
-			class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-			id="contacts"
-			role="tabpanel"
-			aria-labelledby="contacts-tab"
-		>
-			<p class="text-sm text-gray-500 dark:text-gray-400">
-				This is some placeholder content the <strong
-					class="font-medium text-gray-800 dark:text-white"
-					>Contacts tab's associated content</strong
-				>. Clicking another tab will toggle the visibility of this one for the next. The tab
-				JavaScript swaps classes to control the content visibility and styling.
-			</p>
+	<div class="w-full">
+		<div class="relative right-0">
+			<ul
+				class="relative flex list-none flex-wrap rounded-lg bg-blue-gray-50/60 p-1"
+				data-tabs="tabs"
+				role="list"
+			>
+				<li class="z-30 flex-auto text-center">
+					<a
+						class="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out"
+						data-tab-target=""
+						active
+						role="tab"
+						aria-selected="true"
+					>
+						<span class="ml-1">HTML</span>
+					</a>
+				</li>
+				<li class="z-30 flex-auto text-center">
+					<a
+						class="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out"
+						data-tab-target=""
+						role="tab"
+						aria-selected="false"
+					>
+						<span class="ml-1">React</span>
+					</a>
+				</li>
+				<li class="z-30 flex-auto text-center">
+					<a
+						class="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out"
+						data-tab-target=""
+						role="tab"
+						aria-selected="false"
+					>
+						<span class="ml-1">Vue</span>
+					</a>
+				</li>
+				<li class="z-30 flex-auto text-center">
+					<a
+						class="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out"
+						data-tab-target=""
+						role="tab"
+						aria-selected="true"
+					>
+						<span class="ml-1">Angular</span>
+					</a>
+				</li>
+				<li class="z-30 flex-auto text-center">
+					<a
+						class="text-slate-700 z-30 mb-0 flex w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 transition-all ease-in-out"
+						data-tab-target=""
+						role="tab"
+						aria-selected="true"
+					>
+						<span class="ml-1">Svelte</span>
+					</a>
+				</li>
+			</ul>
 		</div>
 	</div>
 
+	<TableUser {users} />
 	<TablePost {posts} />
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,54 +2,54 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}', './src/style/**/*.css'],
 	theme: {
-		screens: {
-			'tablet-sm': '768px',
-			'tablet': '915px',
-			'tablet-max': { max: '915px' },
-			'laptop': '1024px',
-			'desktop': '1280px',
-			'desktop-max': { max: '1280px' },
-			'desktop-xl': '1440px',
-			'desktop-xl-max': { max: '1440px' }
-		},
-		colors: {
-			'transparent': 'transparent',
-			'white': '#FFFFFF',
-			'white-secondary': '#FAF8FF',
-			'green': '#7FD1AE',
-			'blue': '#0390BF',
-			'blue-dark': '#0388B5',
-			'blue-white-light': '#EEF8FC',
-			'night-blue': '#112A46',
-			'night-blue-light': '#eef4fb',
-			'night-blue-dark': '#122D47',
-			'red': '#C31952',
-			'red-light': '#fbdee8',
-			'red-dark': '#a21544',
-			'black': '#000000',
-			'text-contrast': '#112A46',
-			'yellow': '#FFFD92',
-			'alert': '#C31952',
-			'golden-yellow': '#FFDF00',
-			'stategray': '#708090',
-			'bronze': '#cd7f32'
-		},
-		fontSize: {
-			sm: '12px',
-			md: '16px',
-			lg: '20px',
-			xl: '24px',
-			xxl: '30px',
-			40: '40px',
-			50: '50px',
-			60: '60px',
-			80: '80px',
-			90: '90px'
-		},
-		fontFamily: {
-			display: 'Montserrat, sans-serif'
-		},
 		extend: {
+			screens: {
+				'tablet-sm': '768px',
+				tablet: '915px',
+				'tablet-max': { max: '915px' },
+				laptop: '1024px',
+				desktop: '1280px',
+				'desktop-max': { max: '1280px' },
+				'desktop-xl': '1440px',
+				'desktop-xl-max': { max: '1440px' }
+			},
+			colors: {
+				transparent: 'transparent',
+				white: '#FFFFFF',
+				'white-secondary': '#FAF8FF',
+				green: '#7FD1AE',
+				blue: '#0390BF',
+				'blue-dark': '#0388B5',
+				'blue-white-light': '#EEF8FC',
+				'night-blue': '#112A46',
+				'night-blue-light': '#eef4fb',
+				'night-blue-dark': '#122D47',
+				red: '#C31952',
+				'red-light': '#fbdee8',
+				'red-dark': '#a21544',
+				black: '#000000',
+				'text-contrast': '#112A46',
+				yellow: '#FFFD92',
+				alert: '#C31952',
+				'golden-yellow': '#FFDF00',
+				stategray: '#708090',
+				bronze: '#cd7f32'
+			},
+			fontSize: {
+				sm: '12px',
+				md: '16px',
+				lg: '20px',
+				xl: '24px',
+				xxl: '30px',
+				40: '40px',
+				50: '50px',
+				60: '60px',
+				80: '80px',
+				90: '90px'
+			},
+			fontFamily: {
+				display: 'Montserrat, sans-serif'
+			},
 			width: {
 				233: '233px'
 			},
