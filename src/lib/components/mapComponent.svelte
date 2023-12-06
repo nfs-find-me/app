d51c8f61ecd481fe4459c077ce0e292630d06cad
loulou761001 <louis.cherik@gmail.com>
2023-12-06 14:45:59 +0100

    :sparkles: added distance calc

--- src/lib/components/mapComponent.svelte
+++ src/lib/components/mapComponent.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { GeoJSON, Layer, LineLayer, MapLibre, Marker } from 'svelte-maplibre';
 	import type { LngLatLike } from 'svelte-maplibre';
-	import type { PostType } from '../store/types.js';
-	import type GeoJson from 'svelte-maplibre/dist/GeoJSON.svelte';
-	import type { Position } from 'postcss';
+	import type { PostType } from '../../store/types.js';
 	export let post: PostType;
 	let markerCoords = [2.3502761752520267, 48.856836256240854] as LngLatLike;
 	export let showAnswer: boolean;
@@ -11,6 +9,31 @@
 		markerCoords = e.detail.lngLat;
 		console.log('drag', e.detail.lngLat);
 	}
+	function degreesToRadians(degrees: number) {
+		return (degrees * Math.PI) / 180;
+	}
+
+	function distanceInKmBetweenEarthCoordinates(
+		lat1: number,
+		lon1: number,
+		lat2: number,
+		lon2: number
+	) {
+		var earthRadiusKm = 6371;
+
+		var dLat = degreesToRadians(lat2 - lat1);
+		var dLon = degreesToRadians(lon2 - lon1);
+
+		lat1 = degreesToRadians(lat1);
+		lat2 = degreesToRadians(lat2);
+
+		var a =
+			Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+			Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		return earthRadiusKm * c;
+	}
+
 	function getLine() {
 		const geoJsonFeature: GeoJSON.Feature = {
 			type: 'Feature',
@@ -20,6 +43,12 @@
 			},
 			properties: {}
 		};
+		distanceInKmBetweenEarthCoordinates(
+			markerCoords[1],
+			markerCoords[0],
+			post.geolocation?.posY as number,
+			post.geolocation?.posX as number
+		);
 		return geoJsonFeature;
 	}
 </script>
@@ -47,7 +76,7 @@
 			}}
 			class="w-10 h-10 transition-all bg-red-300 text-black rounded-full grid place-items-center"
 		>
-			<img src="../favicon.svg" class="h-10" alt="marker" />
+			<img src="../../../static/favicon.svg" class="h-10" alt="marker" />
 		</Marker>
 		{#if showAnswer === true}
 			<GeoJSON id="test" data={getLine()}
@@ -65,7 +94,7 @@
 				lngLat={[post.geolocation?.posX, post.geolocation?.posY]}
 				class="w-8 h-8 bg-red-300 text-black rounded-full grid place-items-center"
 			>
-				<img src="../favicon.svg" class="h-8" alt="marker" />
+				<img src="../../../static/favicon.svg" class="h-8" alt="marker" />
 			</Marker>
 		{/if}
 	</MapLibre>
