2dd5ac05e38cc5a9283c64aab6426215654c091e
j.pinto <j.pinto@pawa-consulting.com>
2023-12-07 11:13:46 +0100

    add about

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<body class="bg-gray-100 p-8">
+
+    <div class="max-w-2xl mx-auto my-20">
+
+        <h1 class="text-3xl font-bold my-6">À propos de FindMe</h1>
+
+        <p class="mb-4">
+            Bienvenue sur FindMe, la plateforme qui vous permet de retrouver la position géographique des images partagées par la communauté. Notre mission est de créer un espace où les utilisateurs peuvent découvrir l'origine des images qu'ils trouvent intéressantes, en se connectant avec leurs amis et en explorant le monde à travers les photos partagées.
+        </p>
+
+        <p class="mb-4">
+            Notre équipe de développeurs passionnés travaille sans relâche pour rendre FindMe simple, intuitif et amusant. Que vous soyez un voyageur, un amateur de photographie ou simplement curieux, FindMe vous offre la possibilité de plonger dans l'histoire derrière chaque image.
+        </p>
+
+        <h2 class="text-2xl font-bold my-4">Notre Équipe</h2>
+
+        <div class="mb-6">
+            <h3 class="text-lg font-bold mb-2">Jody Pinto Sanhudo</h3>
+            <p class="text-gray-600">SEO de l'application</p>
+        </div>
+        
+        <div class="mb-6">
+            <h3 class="text-lg font-bold mb-2">Maxence Bossin</h3>
+            <p class="text-gray-600">Développeur Frontend de l'application web et expert en SvelteKit</p>
+        </div>
+        
+        <div class="mb-6">
+            <h3 class="text-lg font-bold mb-2">Alexis Briet</h3>
+            <p class="text-gray-600">Développeur Backend de l'application</p>
+        </div>
+        
+        <div class="mb-6">
+            <h3 class="text-lg font-bold mb-2">Louis Cherik</h3>
+            <p class="text-gray-600">Développeur Backend de l'application</p>
+        </div>
+
+        <h2 class="text-2xl font-bold my-4">Contactez-nous</h2>
+
+        <p class="mb-4">
+            Vous avez des questions, des suggestions ou simplement envie de dire bonjour ? N'hésitez pas à nous contacter à l'adresse suivante : FindMe@contact.com
+        </p>
+
+    </div>
+
+</body>
+
